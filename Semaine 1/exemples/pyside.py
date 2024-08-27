8684e1c311852c6c5f7c735126bfefd0d7ec6e2b
Pier Luc Ducharme <dutch80@gmail.com>
2024-08-27 15:57:16 -0400

    Ajout exemples semaine 1

---
+++ Semaine 1/exemples/pyside.py
@@ -0,0 +1,24 @@
+from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # Titre de la fenetre
+        self.setWindowTitle("PySide")
+
+        bouton_fermer = QPushButton("Fermer")
+        # connecter l'evenement du click a la fonction close de la fenetre
+        bouton_fermer.pressed.connect(self.close)
+
+        self.setCentralWidget(bouton_fermer)
+        # Affichage de la fenetre
+        self.show()
+
+# Creation de l'application
+app = QApplication(sys.argv)
+# Creation de la fenetre (la classe ci-haut)
+w = MainWindow()
+# execution
+app.exec()
---
+++ Semaine 1/exemples/tk.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, Button, Label
+
+class Window(Tk):
+
+    def __init__(self):
+        super().__init__()
+        # Titre de la fenetre
+        self.title("Test de Tk")
+        # Ajout d'un texte
+        self.label = Label(text="Cliquez pour fermer")
+        self.label.pack()
+        # Ajout d'un bouton
+        self.button = Button(text="Fermer", command=self.handle_button_press)
+        self.button.pack()
+
+    # Gestion de l'evenement du bouton
+    def handle_button_press(self):
+        self.destroy()
+
+# Creation de la fenetre
+window = Window()
+# Boucle principale
+window.mainloop()
