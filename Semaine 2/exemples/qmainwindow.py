40a961a297931c33b48a5406a3470143f44dfc1f
Pier Luc Ducharme <dutch80@gmail.com>
2024-09-03 15:02:46 -0400

    correction exemples semaine 2

--- Semaine 2/exemples/qmainwindow.py
+++ Semaine 2/exemples/qmainwindow.py
@@ -2,27 +2,39 @@ import sys
 
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QMainWindow, QApplication, QToolBar, QLabel, QStatusBar, QPushButton
-from PySide6.QtGui import QAction, QKeySequence
+from PySide6.QtGui import QAction, QKeySequence, QIcon
 
 
 class FenetrePrincipale(QMainWindow):
     def __init__(self):
         super(FenetrePrincipale, self).__init__()
 
-        label_texte = QLabel("texte")
+        label_texte = QLabel("texteasdfsadfsadfdsaf")
 
         self.setCentralWidget(label_texte)
 
         barre_menus = self.menuBar()
         self.creer_menus(barre_menus)
 
+        icone = QIcon("jouer.png")
+
         bouton_test = QPushButton("Test")
+        bouton_test.setIcon(icone)
         bouton_test.pressed.connect(self.action_executee)
+
+
 
         barre_outils = QToolBar("Barre d'outils")
         barre_outils.setIconSize(QSize(16, 16))
         barre_outils.addWidget(bouton_test)
         self.addToolBar(barre_outils)
+
+        barre_outils_2 = QToolBar("Barre d'outils 2")
+        barre_outils_2.setIconSize(QSize(16, 16))
+        bouton_outils = QPushButton("Outils")
+        bouton_outils.pressed.connect(self.action_executee)
+        barre_outils_2.addWidget(bouton_outils)
+        self.addToolBar(barre_outils_2)
 
         self.barre_statut = QStatusBar(self)
         self.barre_statut.setStatusTip("cliquer un bouton")
